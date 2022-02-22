a056b3bfc8b132fc033fd6846c64dd0b2663b08f
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-22 15:00:29 +0100

    Add files via upload

---
+++ EjerciciosSegundaEvaluacion/AJAX/codificarjson/codificar.php
@@ -0,0 +1,8 @@
+
+<?php
+$a = array("Sara","Imanol","Dani","Antonio","David","Igor", "Naroa", "Christian", "Joseba", "Angel", "Alex", "Dumitru", "Mikel", "Ivan", "Martin");
+
+$miJSON = json_encode($a);
+echo $miJSON;
+
+?>
